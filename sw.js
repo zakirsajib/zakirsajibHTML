60560b9cb486ecab55da0c6cf9b1a73ee96d5f2f
Zakir Sajib <zakirsajib@gmail.com>
2019-01-07 11:05:14 +0000

    Updated PWA

--- sw.js
+++ sw.js
@@ -1,38 +1,99 @@
-const version = "1.0";
-const cacheName = 'zakirsajib-${version}';
-self.addEventListener('install', e => {
+// Copyright 2016 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var dataCacheName = 'zakirSajib-v1';
+var cacheName = 'zakirSajibPWA-final-1';
+var filesToCache = [
+  	'/',
+	'/index.html',
+	'/wp-content/themes/zsonline/assets/images/bg-hero-1.webp',
+	'/wp-content/themes/zsonline/assets/images/bg-milestones-1.webp',
+	'/wp-content/themes/zsonline/assets/images/bg-references-1.webp',
+	'/wp-includes/js/jquery/jquery-1.12.4.js',
+	'/wp-content/themes/zsonline/assets/plugins/lazysizes/ls.bgset.min.js',
+	'/wp-content/themes/zsonline/assets/plugins/lazysizes/lazysizes.min.js',
+	'/wp-content/themes/zsonline/assets/plugins/smoothscroll.js',
+	'/wp-content/themes/zsonline/assets/plugins/bootstrap/js/bootstrap.min.js',
+	'/wp-content/themes/zsonline/static/dist/js/app.min.js',
+	'/wp-content/themes/zsonline/assets/critical.css',
+	'/wp-content/themes/zsonline/assets/noncritical.css',
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
   e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/wp-content/themes/zsonline/assets/images/bg-hero-1.webp',
-        '/wp-content/themes/zsonline/assets/images/bg-milestones-1.webp',
-        '/wp-content/themes/zsonline/assets/images/bg-references-1.webp',
-        '/wp-includes/js/jquery/jquery-1.12.4.js',
-        '/wp-content/themes/zsonline/assets/plugins/lazysizes/ls.bgset.min.js',
-        '/wp-content/themes/zsonline/assets/plugins/lazysizes/lazysizes.min.js',
-        '/wp-content/themes/zsonline/assets/plugins/smoothscroll.js',
-        '/wp-content/themes/zsonline/assets/plugins/bootstrap/js/bootstrap.min.js',
-        '/wp-content/themes/zsonline/static/dist/js/app.min.js',
-        '/wp-content/themes/zsonline/assets/critical.css',
-        '/wp-content/themes/zsonline/assets/noncritical.css',
-      ])
-          .then(() => self.skipWaiting());
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
     })
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
-      .then(response => {
-      return response || fetch(event.request);
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
     })
   );
+  /*
+   * Fixes a corner case in which the app wasn't returning the latest data.
+   * You can reproduce the corner case by commenting out the line below and
+   * then doing the following steps: 1) load app for first time so that the
+   * initial New York City data is shown 2) press the refresh button on the
+   * app 3) go offline 4) reload the app. You expect to see the newer NYC
+   * data, but you actually see the initial data. This happens because the
+   * service worker is not yet activated. The code below essentially lets
+   * you activate the service worker faster.
+   */
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
 });
