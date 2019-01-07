390da8a79bc127a144857bff470eb97cfeb0ccda
Zakir Sajib <zakirsajib@gmail.com>
2019-01-07 07:18:06 +0000

    no message

--- sw.js
+++ sw.js
@@ -3,18 +3,18 @@ self.addEventListener('install', function(e) {
     caches.open('your-magic-cache').then(function(cache) {
       return cache.addAll([
         '/',
-        '/manifest.json',
-        '/wp-content/themes/zsonline/assets/images/bg-hero-1.webp',
-        '/wp-content/themes/zsonline/assets/images/bg-milestones-1.webp',
-        '/wp-content/themes/zsonline/assets/images/bg-references-1.webp',
-        '/wp-includes/js/jquery/jquery-1.12.4.js',
-        '/wp-content/themes/zsonline/assets/plugins/lazysizes/ls.bgset.min.js',
-        '/wp-content/themes/zsonline/assets/plugins/lazysizes/lazysizes.min.js',
-        '/wp-content/themes/zsonline/assets/plugins/smoothscroll.js',
-        '/wp-content/themes/zsonline/assets/plugins/bootstrap/js/bootstrap.min.js',
-        '/wp-content/themes/zsonline/static/dist/js/app.min.js',
-        '/wp-content/themes/zsonline/assets/critical.css',
-        '/wp-content/themes/zsonline/assets/noncritical.css',
+        'manifest.json',
+        'wp-content/themes/zsonline/assets/images/bg-hero-1.webp',
+        'wp-content/themes/zsonline/assets/images/bg-milestones-1.webp',
+        'wp-content/themes/zsonline/assets/images/bg-references-1.webp',
+        'wp-includes/js/jquery/jquery-1.12.4.js',
+        'wp-content/themes/zsonline/assets/plugins/lazysizes/ls.bgset.min.js',
+        'wp-content/themes/zsonline/assets/plugins/lazysizes/lazysizes.min.js',
+        'wp-content/themes/zsonline/assets/plugins/smoothscroll.js',
+        'wp-content/themes/zsonline/assets/plugins/bootstrap/js/bootstrap.min.js',
+        'wp-content/themes/zsonline/static/dist/js/app.min.js',
+        'wp-content/themes/zsonline/assets/critical.css',
+        'wp-content/themes/zsonline/assets/noncritical.css',
       ]);
     })
   );
