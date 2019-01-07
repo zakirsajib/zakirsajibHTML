d77b412f1da3c1105c06736e40b0d94815306d4b
Zakir Sajib <zakirsajib@gmail.com>
2019-01-07 07:15:42 +0000

    no message

--- sw.js
+++ sw.js
@@ -30,7 +30,6 @@ self.addEventListener('fetch', function(event) {
     return;
   }
 
-self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
