dde07c90099404a57186b5b51934ca4b7e89bb2d
Zakir Sajib <zakirsajib@gmail.com>
2019-01-10 06:27:59 +0000

    no message

--- sw.js
+++ sw.js
@@ -4,15 +4,7 @@ var urlsToCache = [
 	'/index.html',
 	'/wp-content/themes/zsonline/assets/images/bg-hero-1.webp',
 	'/wp-content/themes/zsonline/assets/images/bg-milestones-1.webp',
-	'/wp-content/themes/zsonline/assets/images/bg-references-1.webp',
-	'/wp-includes/js/jquery/jquery-1.12.4.js',
-	'/wp-content/themes/zsonline/assets/plugins/lazysizes/ls.bgset.min.js',
-	'/wp-content/themes/zsonline/assets/plugins/lazysizes/lazysizes.min.js',
-	'/wp-content/themes/zsonline/assets/plugins/smoothscroll.js',
-	'/wp-content/themes/zsonline/assets/plugins/bootstrap/js/bootstrap.min.js',
-	'/wp-content/themes/zsonline/static/dist/js/app.min.js',
-	'/wp-content/themes/zsonline/assets/critical.css',
-	'/wp-content/themes/zsonline/assets/noncritical.css'
+	'/wp-content/themes/zsonline/assets/images/bg-references-1.webp'
 ];
 
 self.addEventListener('install', function(event) {
