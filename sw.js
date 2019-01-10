68df68cb31f26101d1a307636759facbafdca7ab
Zakir Sajib <zakirsajib@gmail.com>
2019-01-10 06:32:43 +0000

    no message

--- sw.js
+++ sw.js
@@ -6,6 +6,11 @@ var urlsToCache = [
 	'/wp-content/themes/zsonline/assets/images/bg-milestones-1.webp',
 	'/wp-content/themes/zsonline/assets/images/bg-references-1.webp',
 	'/wp-includes/js/jquery/jquery-1.12.4.js',
+	'/wp-content/themes/zsonline/assets/plugins/lazysizes/ls.bgset.min.js',
+    '/wp-content/themes/zsonline/assets/plugins/lazysizes/lazysizes.min.js',
+    '/wp-content/themes/zsonline/assets/plugins/smoothscroll.js',
+    '/wp-content/themes/zsonline/assets/plugins/bootstrap/js/bootstrap.min.js',
+    '/wp-content/themes/zsonline/static/dist/js/app.min.js'
 ];
 
 self.addEventListener('install', function(event) {
